e19edabd70d0db6d9b3da4a235a0468275665a66
NureniJamiu <nurenijamiu951@gmail.com>
2025-11-07 12:23:46 +0100

    replace white logo with gold logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-50 w-full">
       <div className="flex items-center justify-between px-6 py-4 ">
         <Link to="/" className="flex items-center">
-          <img src="/white-logo.png" alt="Tinad Logo" className="h-16 w-auto object-contain" />
+          <img src="/gold-logo.png" alt="Tinad Logo" className="h-16 w-auto object-contain" />
         </Link>
         <ul className="flex items-center gap-8 bg-black w-full md:w-auto rounded-b-lg py-3 md:py-0">
           {navLinks.map((link) => (

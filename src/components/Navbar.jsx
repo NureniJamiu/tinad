147d9c7d558c8cc8e663b6599e3ebfc2270d163b
NureniJamiu <nurenijamiu951@gmail.com>
2025-11-07 07:39:20 +0100

    style: adjust logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-50 w-full">
       <div className="flex items-center justify-between px-6 py-4 ">
         <Link to="/" className="flex items-center">
-          <img src="/white-logo.png" alt="Tinad Logo" className="h-12 w-auto object-contain" />
+          <img src="/white-logo.png" alt="Tinad Logo" className="h-16 w-auto object-contain" />
         </Link>
-        <ul className="flex items-center gap-8 bg-black w-full md:w-auto">
+        <ul className="flex items-center gap-8 bg-black w-full md:w-auto rounded-full">
           {navLinks.map((link) => (
             <li key={link.name}>
               <Link
